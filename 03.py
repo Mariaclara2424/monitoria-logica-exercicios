ad4ee776c1a8c5e386214542ff52f5378f6fc189
Mariaclara2424 <pedrosamariaclara5@gmail.com>
2025-09-10 18:46:09 -0300

    resolução da questao

--- 03.py
+++ 03.py
@@ -1,8 +1,10 @@
 # Exercício 3: Par ou ímpar
-# Complete a condição para verificar se o número é par ou ímpar
+# Complete a condição para verificar se o número é par ou ímp
 
-"""
-    - O programa deve pedir ao usuário que digite um número inteiro.
-    - Após isso, o programa deve verificar se o número digitado é
-      par ou ímpar.
-"""
+numero = int(input('Digite um número: '))
+
+if numero % 2 == 0 :
+        print('Esse número é par')
+        
+else :
+        print('Esse número é ímpar')
